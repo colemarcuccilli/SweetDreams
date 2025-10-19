d5dce6641d86fbf58559cf6b776dc812fb6e487b
Cole Marcuccilli <cole@marcuccilli.com>
2025-10-18 20:21:15 -0400

    Add data-scroll-behavior attribute to fix Next.js warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body className={ibmPlexMono.className}>
         <Nav />
         <main className="min-h-screen">{children}</main>
